d348a04a381b68366438d3aaf7ec9376be9435c5
curtis.mayberry <curtisma3@gmail.com>
2022-08-14 11:23:30 -0700

    fixed python test runner

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     run_tests_path = tests_path / "run_tests.ils"
     src_init_path = tests_path /".." / "virtue" / "virtue.init.ils"
     ws = Workspace.open('ids_dev-mayberc')
-    print("setup")
-    env = env = ws["theEnvironment"]()
-    print(env)
-    skill_import = ws.__.x
-    #oad_result = ws["load"](str(src_init_path))
-    #ws["toplevel"](Symbol("ils"))
-    print(skill_import)
+    skill_import = ws.__.Import
     test_pkg = skill_import[Symbol("Test")]
-    print("run_tests")
-    result = test_pkg.run_directory_suite(str(tests_path))
-    print(result)
+    print(f"\nRunning tests in:\n  {run_tests_path}")
+    run_dir_tests = test_pkg[8]
+    result = run_dir_tests(str(tests_path))
+    function_count = result["FunctionCount"]()
+    pass_count = result["FunctionPassCount"]()
+    if result["pass"]:
+        print(f"All {pass_count} tests passed! ")
+    else:
+        print(f"Some tests failds:\n  {pass_count} passed out of {function_count}\n")
