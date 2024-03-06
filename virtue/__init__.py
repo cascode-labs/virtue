5a80ab7a1d117e8a54a387c211720adc4b46c408
curtis.mayberry <curtisma3@gmail.com>
2024-03-06 23:15:57 +0000

    v0.7.0

--- virtue/__init__.py
+++ virtue/__init__.py
@@ -5,6 +5,6 @@ from .SchUtility import *
 from .ConfigUtility import *
 from .skill_package.metadata_data import SKillPackageMetadata
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 
 hookimpl = pluggy.HookimplMarker("virtue")
