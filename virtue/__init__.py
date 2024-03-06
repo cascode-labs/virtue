87e0af755fa6db70ceab9f87022f3abe4189c40d
curtis.mayberry <curtisma3@gmail.com>
2024-03-06 22:23:32 +0000

    fix python package

--- virtue/__init__.py
+++ virtue/__init__.py
@@ -1,7 +1,6 @@
 """Cadence Virtuoso SKILL and Python library"""
 import pluggy
 
-from .LibUtility import *
 from .SchUtility import *
 from .ConfigUtility import *
 from .skill_package.metadata_data import SKillPackageMetadata
